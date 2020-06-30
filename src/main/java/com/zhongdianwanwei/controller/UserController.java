0dbe2e3490932e7ea1e5847222170142ce03b46c
865739088 <865739088@qq.com>
2020-06-30 15:36:37 +0800

    金鑫：增加swagger组件功能

--- src/main/java/com/zhongdianwanwei/controller/UserController.java
+++ src/main/java/com/zhongdianwanwei/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.zhongdianwanwei.controller;
 
 import com.zhongdianwanwei.service.IUserService;
-import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-@Api("用户相关Api")
+
 @RestController
 public class UserController {
 
